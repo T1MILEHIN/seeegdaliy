fd587a5a6ea24cc39d78c82a31129e4eabcd846b
Timi <tunmisebabade@gmail.com>
2024-11-21 16:01:14 +0100

    first-commit

--- js/script.js
+++ js/script.js
@@ -1,82 +1,76 @@
-(function($) {
+(function ($) {
+  "use strict";
+  $(document).ready(function () {
+    // masonoary //
+    initIsotope();
+    // lightbox
+    lightbox.option({
+      resizeDuration: 200,
+      wrapAround: true,
+      fitImagesInViewport: true,
+    });
 
-    "use strict";
+    /* swiper */
+    var testimonialSwiper = new Swiper(".book-swiper", {
+      slidesPerView: "auto",
+      spaceBetween: 20,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      }
+    });
+  });
+  // init Isotope
+  var initIsotope = function () {
+    $(".grid").each(function () {
+      // $('.grid').imagesLoaded( function() {
+      // images have loaded
+      var $buttonGroup = $(".button-group");
+      var $checked = $buttonGroup.find(".is-checked");
+      var filterValue = $checked.attr("data-filter");
 
-    $(document).ready(function() {
-      
-      // masonoary //
-
-      initIsotope();
-
-      // lightbox
-
-      lightbox.option({
-        'resizeDuration': 200,
-        'wrapAround': true,
-        'fitImagesInViewport': true
-      })
-      
-      /* swiper */
-      
-
-      var testimonialSwiper = new Swiper(".testimonial-swiper", {
-        spaceBetween: 20,
-        pagination: {
-            el: ".testimonial-swiper-pagination",
-            clickable: true,
-          },
-        breakpoints: {
-          0: {
-            slidesPerView: 1,
-          },
-          800: {
-            slidesPerView: 3,
-          },
-          1400: {
-            slidesPerView: 3,
-          }
-        },
+      var $grid = $(".grid").isotope({
+        itemSelector: ".portfolio-item",
+        // layoutMode: 'fitRows',
+        filter: filterValue,
       });
 
-    }); // End of a document ready
+      // bind filter button click
+      $(".button-group").on("click", "a", function (e) {
+        e.preventDefault();
+        filterValue = $(this).attr("data-filter");
+        $grid.isotope({ filter: filterValue });
+      });
 
-  // init Isotope
-  var initIsotope = function() {
-    
-    $('.grid').each(function(){
-
-      // $('.grid').imagesLoaded( function() {
-        // images have loaded
-        var $buttonGroup = $( '.button-group' );
-        var $checked = $buttonGroup.find('.is-checked');
-        var filterValue = $checked.attr('data-filter');
-  
-        var $grid = $('.grid').isotope({
-          itemSelector: '.portfolio-item',
-          // layoutMode: 'fitRows',
-          filter: filterValue
+      // change is-checked class on buttons
+      $(".button-group").each(function (i, buttonGroup) {
+        $buttonGroup.on("click", "a", function () {
+          $buttonGroup.find(".is-checked").removeClass("is-checked");
+          $(this).addClass("is-checked");
         });
-    
-        // bind filter button click
-        $('.button-group').on( 'click', 'a', function(e) {
-          e.preventDefault();
-          filterValue = $( this ).attr('data-filter');
-          $grid.isotope({ filter: filterValue });
-        });
-    
-        // change is-checked class on buttons
-        $('.button-group').each( function( i, buttonGroup ) {
-          $buttonGroup.on( 'click', 'a', function() {
-            $buttonGroup.find('.is-checked').removeClass('is-checked');
-            $( this ).addClass('is-checked');
-          });
-        });
+      });
       // });
+    });
+  };
+
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 50) {
+      $("#header").addClass("scrolled");
+    } else {
+      $("#header").removeClass("scrolled");
+    }
+  });
+  const ssPreloader = function () {
+    $("html").addClass('ss-preload');
+    $WIN.on('load', function () {
+      $("#loader").fadeOut("slow", function () {
+        $("#preloader").delay(300).fadeOut("slow");
+      });
+      $("html").removeClass('ss-preload');
+      $("html").addClass('ss-loaded');
 
     });
-  }
-
-
-
-
+  };
+  ssPreloader();
 })(jQuery);
